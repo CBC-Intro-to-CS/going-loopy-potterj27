da56c85ae4acdbc1e779a3c7f452b3b45f5d5855
Joey <potterj@cbchs.org>
2024-09-19 22:36:34 -0500

    Made loops to make lists do calculations and count by odd numbers

---
+++ going_loopy_potter.py
@@ -0,0 +1,22 @@
+
+     
+# age = int(input("Enter age: "))
+# if age % 2==1:
+#     current_value =1
+# while current_value <=age:
+#         print(current_value)
+#         current_value +=2 
+
+# sandwich_ingredients= ["mayo", "bread", "cheese", "pickle", "ham"] 
+# for x in range (1,6):
+#      print(f"{x}. {sandwich_ingredients[x - 1]}") 
+
+
+# earth_weight = float(input("Enter your weight in kilos: "))
+# moon_gravity = 0.165
+# for x in range(1, 16):
+#     moon_weight = earth_weight * moon_gravity
+#     print(f"Year {x}: Your weight on the moon is {moon_weight:.2f} kg.") 
+#     earth_weight += 1
+
+
